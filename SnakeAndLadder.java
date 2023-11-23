7e5db6e6782d4e23e96e88aa02d61784c7ff3446
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-23 13:15:39 +0530

    Added : initialized snake and ladder simulator

---
+++ SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.SnakeAndLadderSimulator;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("!!! Snake And Ladder Simulator !!!");
+    }
+}
